60fa8a9d5ea47eecb0e4cda13aa4d2f91172747b
prarthanaxdev <prarthanab@mindfiresolutions.com>
2021-02-24 21:07:06 +0530

    BACKEND

--- index.js
+++ index.js
@@ -35,14 +35,30 @@ app.post('/setData', function (req, res) {
         let title= ob.title
         let uid = ob.uid
 
-        let name='todo'+i
+        let name='todo-'+i
         client.hmset(name,"id",id,"title",title, "uid",uid)
     })
+    res.send("DATA SAVED")
 })
 
-
-app.post('/deleteTodo', function (req, res) {
+app.post('/setSubtodo', function (req, res) {
     let body = req.body
+
+    body.items.map((ob,i)=>{
+        let todoId = ob.id
+        let subTodoId = ob.subTodoId
+        let subTodo = ob.subTodo
+
+        let name='Subtodo-'+i
+
+        client.hmset(name,"id",todoId,"subTodo",subTodo, "subTodoId",subTodoId)
+    })
+
+    res.send("DATA SAVED")
+})
+
+app.get('/getData/:id', function (req, res) {
+    var arr=[]
 
     client.keys("*", function(err, keys) {
         if (err) {
@@ -50,24 +66,40 @@ app.post('/deleteTodo', function (req, res) {
         }
 
         for(var i = 0, len = keys.length; i < len; i++) {
-            let id = body.id.toString().length
-            
-            if(id > 1){
-                keys.map((ob,i)=>{
-                    console.log(ob)
-                    client.hgetall(ob, function(err, object) {
-                        if(object.id == body.id){
-                            client.hdel(ob,id, "title","uid",redis.print )
-                        }
-                    });
-                })
-            }
-            else {
-                console.log("HERE")
+            if(keys[i].split("-")[0] == "todo"){   
+                client.hgetall(keys[i], function(err, object) {
+                    arr.push(object)
+                });
             }
         }
+
+        setTimeout(() => {
+            res.send(arr)
+        }, 1000);
     }); 
-    // client.hdel()
+})
+
+app.get('/getSubtodos/:id', function (req, res) {
+    var arr=[]
+
+    client.keys("*", function(err, keys) {
+        if (err) {
+          return callback(err);
+        }
+
+        for(var i = 0, len = keys.length; i < len; i++) {
+            if(keys[i].split("-")[0] == "Subtodo"){   
+                client.hgetall(keys[i], function(err, object) {
+                    if(object.id == req.params.id){
+                        arr.push(object)
+                    }
+                });
+            }
+        }
+        setTimeout(() => {
+            res.send(arr)
+        }, 1000);
+    }); 
 })
 
 app.post('/addNewTodo', function (req, res) {
@@ -78,27 +110,96 @@ app.post('/addNewTodo', function (req, res) {
           return callback(err);
         }
 
-        let name='todo'+keys.length
-        client.hmset(name,"title",body.title, "uid",body.userId,redis.print)
+        let name='todo-'+keys.length
+        client.hmset(name,"title",body.title,"id", keys.length,"uid",body.userId,redis.print)
     }); 
+
+    res.send("DATA SAVED")
 })
 
-app.get('/getData/:id', function (req, res) {
-    var arr=[]
+app.post('/addNewSubTodo', function (req, res) {
+    let body = req.body
 
     client.keys("*", function(err, keys) {
         if (err) {
           return callback(err);
         }
-        for(var i = 0, len = keys.length; i < len; i++) {
-            client.hgetall(keys[i], function(err, object) {
-                arr.push(object)
-            });
-        }
-        setTimeout(() => {
-            res.send(arr)
-        }, 1000);
+
+        let name='Subtodo-'+keys.length
+        client.hmset(name,"subTodo",body.subTodo,"subTodoId",keys.length,"id",body.todoId,redis.print)
     }); 
+
+    res.send("DATA SAVED")
+})
+
+app.post('/deleteTodo', function (req, res) {
+    let body = req.body
+
+    client.keys("*", function(err, keys) {
+        if (err) {
+          return callback(err);
+        }
+
+        for(var i = 0, len = keys.length; i < len; i++) {            
+            keys.map((ob,i)=>{
+                client.hgetall(ob, function(err, object) {
+                    if(object.id == body.id){
+                        client.hdel(keys[i], "title","uid","id",redis.print )
+                    }
+                });
+            })
+        }
+    }); 
+    res.send("DELETED")
+})
+
+app.post('/updateTodo', function (req, res) {
+    let body = req.body
+
+    client.keys("*", function(err, keys) {
+        if (err) {
+          return callback(err);
+        }
+
+        for(var i = 0, len = keys.length; i < len; i++) {            
+            keys.map((ob,i)=>{
+                client.hgetall(ob, function(err, object) {
+
+                    console.log("&&&&&&&&&&&", object,"&&&&&&", body)
+                    if(object.id == body.id){
+                        client.hdel(keys[i], "title")
+                        client.hmset(keys[i],"title",body.title,redis.print)
+                    }
+                });
+            })
+        }
+
+        
+    }); 
+
+    res.send("DATA SAVED")
+})
+
+
+app.post('/deleteSubTodo', function (req, res) {
+    let body = req.body
+
+    client.keys("*", function(err, keys) {
+        if (err) {
+          return callback(err);
+        }
+
+        for(var i = 0, len = keys.length; i < len; i++) {            
+            keys.map((ob,i)=>{
+                client.hgetall(ob, function(err, object) {
+                    if(object.subTodoId == body.id){
+                        client.hdel(keys[i], "subTodo","subTodoId","id",redis.print )
+                    }
+                });
+            })
+        }
+    }); 
+    res.send("DELETED")
 })
 
 app.post('/removeSession', function(req,res){
